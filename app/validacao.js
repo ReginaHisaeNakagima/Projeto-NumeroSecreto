1b9f4ae2e16e4ad5dea573f7d3758165898161bd
HisaeNakagima <1823289@aluno.univesp.br>
2023-07-27 08:22:49 -0300

    Criação do botão Jogar novamente

--- app/validacao.js
+++ app/validacao.js
@@ -15,6 +15,8 @@ function verificaSeOChutePossuiUmValorValido(chute) {
     document.body.innerHTML = `
     <h2>Você acertou!</h2>
     <h3>O número secreto era ${numeroSecreto}</h3>
+
+    <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
     `;
   } else if (numero > numeroSecreto) {
     elementoChute.innerHTML +=
@@ -34,3 +36,9 @@ function numeroForMaiorOuMenorQueOValorPermitido(numero) {
 }
 
 recognition.addEventListener("end", () => recognition.start());
+
+document.body.addEventListener("click", (e) => {
+  if (e.target.id == "jogar-novamente") {
+    window.location.reload();
+  }
+});
